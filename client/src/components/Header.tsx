a75ca4e90829d62c15eb089db061c1fda984ae19
alexanderreiprich <al3xz0n3@gmail.com>
2025-05-27 12:54:22 +0200

    feat: database, card redesign

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -26,10 +26,10 @@ export default function Header() {
 
         <Box className="navigation-container">
           <Button color="inherit" onClick={() => navigate("/search")}>
-            Create
+            Search
           </Button>
           <Button color="inherit" onClick={() => navigate("/community")}>
-            Search
+            Community
           </Button>
         </Box>
 
